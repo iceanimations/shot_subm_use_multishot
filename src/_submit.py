d4b366383bfebc8b2ba8c79f642a897a5d012ae4
Qurban Ali <qurban_ali36@yahoo.com>
2014-09-01 17:55:03 +0500

    ui files created and designed, edit icon placed in folder

--- src/_submit.py
+++ src/_submit.py
@@ -11,7 +11,8 @@ import os.path as osp
 
 root_path = osp.dirname(osp.dirname(__file__))
 ui_path = osp.join(root_path, 'ui')
+icon_path = osp.join(root_path, 'icons')
 
-Form, Base = uic.loadUiType()
+Form, Base = uic.loadUiType(osp.join(ui_path, 'submitter.ui'))
 class Submitter(Form, Base):
     pass
