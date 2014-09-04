66b9a61efd2d16ba5dda4f795f22e57792a8c569
Qurban Ali <qurban_ali36@yahoo.com>
2014-09-04 12:48:24 +0500

    following features are working: new Item creation, camera attribute setting, items editlabe, removable, openable, playblast export in batch

--- src/_submit.py
+++ src/_submit.py
@@ -6,11 +6,15 @@ Created on Sep 1, 2014
 import site
 site.addsitedir(r'R:\Pipe_Repo\Users\Qurban\utilities')
 from uiContainer import uic
-from PyQt4.QtGui import QIcon, QMessageBox
-import os
+from PyQt4.QtGui import QIcon, QMessageBox, QFileDialog
+from PyQt4 import QtCore
 import os.path as osp
 import qtify_maya_window as qtfy
 import pymel.core as pc
+import re
+import subprocess
+import backend
+reload(backend)
 
 root_path = osp.dirname(osp.dirname(__file__))
 ui_path = osp.join(root_path, 'ui')
@@ -31,16 +35,45 @@ class Submitter(Form, Base):
         self.searchBox.setStyleSheet(style_sheet)
         
         self.addButton.clicked.connect(self.showForm)
+        self.selectAllButton.clicked.connect(self.selectAll)
+        self.searchBox.textChanged.connect(self.searchShots)
+        self.searchBox.returnPressed.connect(lambda: self.searchShots
+                                             (str(self.searchBox.text())))
+        self.exportButton.clicked.connect(self.export)
+        self.poplate()
+        
+    def searchShots(self, text):
+        text = str(text)
+        for item in self.items:
+            if text in item.getTitle():
+                item.show()
+            else:
+                item.hide()
+        
+    def selectAll(self):
+        for item in self.items:
+            item.setChecked(self.selectAllButton.isChecked())
+            
+    def itemClicked(self):
+        flag = True
+        for item in self.items:
+            if not item.isChecked():
+                flag = False
+                break
+        self.selectAllButton.setChecked(flag)
         
     def showForm(self):
-        form = ShotForm(self)
-        form.show()
+        ShotForm(self).show()
     
     def addItem(self, item):
         self.items.append(item)
         
     def addItems(self, items):
         self.items.extend(items)
+        
+    def removeItem(self, item):
+        self.items.remove(item)
+        item.deleteLater()
         
     def clear(self):
         for item in self.items:
@@ -51,12 +84,19 @@ class Submitter(Form, Base):
         return self.items
     
     def poplate(self):
-        pass
+        for cam in [x for x in pc.ls(type='camera')
+                    if x.orthographic.get() == False]:
+            if pc.attributeQuery('shotInfo', node=cam, exists=True):
+                data = eval(cam.shotInfo.get())
+                self.createItem(data)
 
     def editItem(self, item):
         ShotForm(self, item).show()
     
     def createItem(self, data):
+        for itm in self.items:
+            if itm.getCamera() == data['camera']:
+                self.removeItem(itm)
         item = Item(self)
         item.setTitle(data['name'])
         item.setCamera(data['camera'])
@@ -64,6 +104,18 @@ class Submitter(Form, Base):
         item.setPath(data['path'])
         self.items.append(item)
         self.layout.addWidget(item)
+        item.setChecked(self.selectAllButton.isChecked())
+        
+
+    def export(self):
+        data = {}
+        for item in self.items:
+            data.clear()
+            if item.isChecked():
+                data['start'] = item.getFrame().split()[0]
+                data['end'] = item.getFrame().split()[-1]
+                data['path'] = osp.join(item.getPath(), item.getTitle())
+                backend.playblast(data)
         
     def closeEvent(self, event):
         self.deleteLater()
@@ -75,16 +127,24 @@ class ShotForm(Form1, Base1):
         super(ShotForm, self).__init__(parent)
         self.setupUi(self)
         self.parentWin = parent
+        self.addCameras()
         self.item = item
         if item:
             self.createButton.setText('Edit')
+            self.populate()
         self.startFrame = None
         self.endFrame = None
-        self.addCameras()
         
         self.cameraBox.activated.connect(self.handleCameraBox)
         self.createButton.clicked.connect(self.create)
         self.keyFrameButton.clicked.connect(self.handleKeyFrameClick)
+        self.browseButton.clicked.connect(self.browseFolder)
+        
+    def browseFolder(self):
+        path = QFileDialog.getExistingDirectory(self, 'Select Folder', '',
+                                                QFileDialog.ShowDirsOnly)
+        if path:
+            self.pathBox.setText(path)
         
     def handleCameraBox(self, camera):
         camera = str(camera)
@@ -99,7 +159,15 @@ class ShotForm(Form1, Base1):
                                 if cam.orthographic.get() == False])
         
     def populate(self):
-        pass
+        self.nameBox.setText(self.item.getTitle())
+        camera = self.item.getCamera()
+        for index in range(self.cameraBox.count()):
+            if camera == str(self.cameraBox.itemText(index)):
+                self.cameraBox.setCurrentIndex(index)
+                break
+        self.startFrameBox.setValue(int(self.item.getFrame().split()[0]))
+        self.endFrameBox.setValue(int(self.item.getFrame().split()[-1]))
+        self.pathBox.setText(self.item.getPath())
     
     def getKeyFrame(self):
         camera = pc.PyNode(str(self.cameraBox.currentText()))
@@ -152,24 +220,65 @@ class ShotForm(Form1, Base1):
         if self.item:
             self.item.setTitle(name)
             self.item.setCamera(camera)
-            self.item.setFrame(start +' to '+ end)
+            self.item.setFrame(str(start) +' to '+ str(end))
             self.item.setPath(path)
         else:
             self.parentWin.createItem(data)
+            
+        camera = pc.PyNode(camera)
+        if not pc.attributeQuery('shotInfo', node=camera, exists=True):
+            pc.addAttr(camera, ln='shotInfo', sn='shin', dt='string',
+                       h=True)
+#         else:
+#             btn = showMessage(self, title='Shot Exists', msg='A shot named '+
+#                         eval(camera.shotInfo.get())['name'] +' already '+
+#                         'exists on this camera', ques='Do you want to '+
+#                         'replace the existing shot?', icon=QMessageBox.Question,
+#                         btns=QMessageBox.Yes|QMessageBox.No)
+#             if btn == QMessageBox.Yes:
+        camera.shotInfo.set(str(data))
+#             else:
+#                 pass
+        self.accept()
         
     def closeEvent(self, event):
         self.deleteLater()
 
 Form2, Base2 = uic.loadUiType(osp.join(ui_path, 'item.ui'))
 class Item(Form2, Base2):
+    
+    version = int(re.search('\\d{4}', pc.about(v=True)).group())
+    if version < 2014:
+        clicked = QtCore.pyqtSignal()
+    else:
+        clicked = QtCore.Signal()
+    
     def __init__(self, parent=None):
         super(Item, self).__init__(parent)
         self.setupUi(self)
         self.parentWin = parent
         
         self.editButton.setIcon(QIcon(osp.join(icon_path, 'ic_edit.png')))
+        self.deleteButton.setIcon(QIcon(osp.join(icon_path, 'ic_delete.png')))
         
         self.editButton.clicked.connect(self.edit)
+        self.clicked.connect(self.parentWin.itemClicked)
+        self.deleteButton.clicked.connect(self.delete)
+        self.browseButton.clicked.connect(self.openLocation)
+        
+    def openLocation(self):
+        subprocess.call('explorer %s'%self.getPath(), shell=True)
+        
+    def delete(self):
+        btn = showMessage(self, title='Delete Shot', msg='Are you sure, delete '
+                    +'"'+ self.getTitle() +'"?', icon=QMessageBox.Question,
+                    btns=QMessageBox.Yes|QMessageBox.No)
+        if btn == QMessageBox.Yes:
+            pc.PyNode(self.getCamera()).shotInfo.delete()
+            self.deleteLater()
+            self.parentWin.removeItem(self)
+        else:
+            pass
         
     def setTitle(self, title):
         self.nameLabel.setText(title)
@@ -206,6 +315,7 @@ class Item(Form2, Base2):
         
     def mouseReleaseEvent(self, event):
         self.toggleSelection()
+        self.clicked.emit()
     
     def edit(self):
         self.parentWin.editItem(self)
---
+++ src/backend/_backend.py
@@ -0,0 +1,7 @@
+import pymel.core as pc
+
+def playblast(data):
+    pc.playblast(st=data['start'], et=data['end'], f=data['path'], fo=True,
+                 quality=100, w=1280, h=720, compression='MS-CRAM', percent=100,
+                 format='avi', sequenceTime=0, clearCache=True,
+                 showOrnaments=True, fp=4)
