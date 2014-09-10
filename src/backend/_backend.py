f9f80c5081c58644873f27d99e4a6a747a0b2b53
Talha Ahmed <talha.ahmed@gmail.com>
2014-09-10 18:34:33 +0500

    Cams by Transforms, OffScreen rendering and path remember

--- src/backend/_backend.py
+++ src/backend/_backend.py
@@ -4,4 +4,4 @@ def playblast(data):
     pc.playblast(st=data['start'], et=data['end'], f=data['path'], fo=True,
                  quality=100, w=1280, h=720, compression='MS-CRAM', percent=100,
                  format='avi', sequenceTime=0, clearCache=True, viewer=False,
-                 showOrnaments=True, fp=4)
+                 showOrnaments=True, fp=4, offScreen=True)
