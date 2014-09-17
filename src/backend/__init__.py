2d7f1714e668a76742ad62121e2a1e837cfec8c8
Talha Ahmed <talha.ahmed@gmail.com>
2014-09-17 11:28:34 +0500

    Ready to integrate

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,2 +1,14 @@
 import _backend
 reload(_backend)
+
+import shotactions
+reload(shotactions)
+
+import shotplaylist
+reload(shotplaylist)
+
+import playblast
+reload(playblast)
+
+Playlist = shotplaylist.Playlist
+PlayblastExport = playblast.PlayblastExport
