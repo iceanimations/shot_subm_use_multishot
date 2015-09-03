06e5d51f012e492b88f9bbc9b9e01466ebf9282a
Talha Ahmed <talha.ahmed@gmail.com>
2015-09-03 18:39:47 +0500

    bug removed: timeline runs even if nano is not selected

--- src/backend/cacheexport.py
+++ src/backend/cacheexport.py
@@ -220,8 +220,12 @@ class CacheExport(Action):
 
     def exportAnimatedTextures(self, conf, local=False):
         ''' bake export animated textures from the scene '''
+        textures_exported = False
 
         if not self.get('objects'):
+            return False
+        animatedTextures = self.getAnimatedTextures(conf)
+        if not animatedTextures:
             return False
 
         tempFilePath = osp.join(self.tempPath, 'tex')
@@ -233,13 +237,12 @@ class CacheExport(Action):
         end_time = int(self._item.getOutFrame())
         rx = conf['texture_resX']
         ry = conf['texture_resY']
-        textures_exported = False
 
         for curtime in range(start_time, end_time+1):
             num = '%04d'%curtime
             pc.currentTime(curtime, e=True)
 
-            for name, attr in self.getAnimatedTextures(conf):
+            for name, attr in animatedTextures:
                 fileImageName = osp.join(tempFilePath,
                         '.'.join([name, num, 'png']))
                 newobj = pc.convertSolidTx(attr, samplePlane=True, rx=rx, ry=ry,
